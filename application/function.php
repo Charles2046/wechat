eadcae567cac0ae6cb462f29e88840310ae98a23
charles2999 <65605807@qq.com>
2017-09-01 18:38:32 +0800

    201709011838

--- application/function.php
+++ application/function.php
@@ -23,6 +23,166 @@ function get_arr_column($arr, $key_name)
 	return $arr2;
 }
 
+function parse_url_param($str)
+{
+	$data = array();
+	$str = explode('?', $str);
+	$str = end($str);
+	$parameter = explode('&', $str);
+	foreach ($parameter as $val) {
+		$tmp = explode('=', $val);
+		$data[$tmp[0]] = $tmp[1];
+	}
+	return $data;
+}
+
+function array_sort($arr, $keys, $type = 'desc')
+{
+	$key_value = $new_array = array();
+	foreach ($arr as $k => $v) {
+		$key_value[$k] = $v[$keys];
+	}
+	if ($type == 'asc') {
+		asort($key_value);
+	} else {
+		arsort($key_value);
+	}
+	reset($key_value);
+	foreach ($key_value as $k => $v) {
+		$new_array[$k] = $arr[$k];
+	}
+	return $new_array;
+}
+
+function array_multi2single($array)
+{
+	static $result_array = array();
+	foreach ($array as $value) {
+		if (is_array($value)) {
+			array_multi2single($value);
+		} else {
+			$result_array[] = $value;
+		}
+	}
+	return $result_array;k
+}
+
+function friend_date($time)
+{
+	if (!$time) {
+		return false;
+	}
+	$fdate = '';
+	$d = time() - intval($time);
+	$ld = $time - mktime(0, 0, 0, 0, 0, date('Y'));
+	$md = $time - mktime(0, 0, 0, date('m'), 0, date('Y'));
+	$byd = $time - mktime(0, 0, 0, date('m'), date('d') -2, date('Y'));
+	$yd = $time - mktime(0, 0, 0,date('m'), date('d') -1, date('Y'));
+	$dd = $time - mktime(0, 0, 0,date('m'), date('d'), date('Y'));
+	$td = $time - mktime(0, 0, 0,date('m'), date('d') + 1, date('Y'));
+	$atd = $time - mktime(0, 0, 0,date('m'), date('d') + 2, date('Y'));
+	if ($d == 0) {
+		$fdate = '刚刚';
+	} else {
+		switch($d) {
+			case $d < $atd:
+				$fdate = date('Y年m月d日', $time);
+				break;
+			case $d < $td:
+				$fdate = '后天' . date('H:i', $time);
+				break;
+			case $d < 0:
+				$fdate = '明天' . date('H:i', $time);
+				break;
+			case $d < 60:
+				$fdate = $d . '秒前';
+				break;
+			case $d < 3600:
+				$fdate = floor($d / 60) . '分钟前';
+				break;
+			case $d < $dd:
+				$fdate = floor($d / 3600) . '小时前';
+				break;
+			case $d < $yd:
+				$fdate = '昨天' . date('H:i', $time);
+				break;
+			case $d < $byd:
+				$fdate = '前天' . date('H:i', $time);
+				break;
+			case $d < $md:
+				$fdate = date('m月d日 H:i', $time);
+				break;
+			case $d < $ld:
+				$fdate = date('m月d日', $time);
+				break;
+			default:
+				$fdate = date('Y年m月d日', $time);
+				break;
+		}
+	}
+	return $fdate;
+}
+
+function arrayRes($status, $info, $url = "")
+{
+	return array(
+		"status" => $status,
+		"info" => $info,
+		"url" => $url
+	);
+}
+
+function get_id_val($arr, $key_name, $key_name2)
+{
+	$arr2 = array();
+	foreach ($arr as $key => $val) {
+		$arr2[$val[$key_name]] = $val[$key_name2];
+	}
+	return $arr2;
+}
+
+function serverIP()
+{
+	return gethostbyname($_SERVER["SERVER_NAME"]);
+}
+
+function recurse_copy($src, $dst)
+{
+	$now = time();
+	$dir = opendir($src);
+	@mkdir($dst);
+	while (false !== $file = readdir($dir)) {
+		if (($file != '.') && ($file != '..')) {
+			if (is_dir($src . '/' . $file)) {
+				recurse_copy($src . '/' . $file, $dst . '/' . $file);
+			} else {
+				if (file_exists($dst . DIRECTORY_SEPARATOR . $file)) {
+					if (!is_writeable($dst . DIRECTORY_SEPARATOR . $file)) {
+						exit($dst . DIRECTORY_SEPARATOR . $file . '不可写');
+					}
+					@unlink($dst . DIRECTORY_SEPARATOR . $file);
+				}
+				if (file_exists($dst . DIRECTORY_SEPARATOR . $file)) {
+					@unlink($dst . DIRECTORY_SEPARATOR . $file);
+				}
+				$copyrt = copy($src . DIRECTORY_SEPARATOR . $file, $dst . DIRECTORY_SEPARATOR . $file);
+				if (!$copyrt) {
+					echo 'copy ' . $dst . DIRECTORY_SEPARATOR . $file . ' failed<br />';
+				}
+			}
+			closedir($dir);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
 /**
  * 判断当前访问的用户是 PC端 还是 手机端 返回true 为手机端 false 为PC 端
  *
